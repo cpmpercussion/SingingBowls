008bfbf365b9449d4eae5b3ae854420f41cfad45
cpmpercussion <cpm@charlesmartin.com.au>
2014-01-20 15:19:06 +1100

    Initial Commit

---
+++ Chorale/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Chorale
+//
+//  Created by Charles Martin on 20/01/2014.
+//  Copyright (c) 2014 Charles Martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Chorale/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Chorale
+//
+//  Created by Charles Martin on 20/01/2014.
+//  Copyright (c) 2014 Charles Martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
