011ff71d70dad27cab3fdc1c95c7461a229c26cb
cpmpercussion <cpm@charlesmartin.com.au>
2014-06-17 16:05:11 +1000

    New files for Metatone Ensemble view. This view should have the different metatone players represented by little semi transparent circles.

---
+++ Chorale/MetatoneEnsembleView.h
@@ -0,0 +1,13 @@
+//
+//  MetatoneEsembleView.h
+//  SingingBowls
+//
+//  Created by Charles Martin on 17/06/2014.
+//  Copyright (c) 2014 Charles Martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MetatoneEnsembleView : UIView
+
+@end
---
+++ Chorale/MetatoneEnsembleView.m
@@ -0,0 +1,23 @@
+//
+//  MetatoneEsembleView.m
+//  SingingBowls
+//
+//  Created by Charles Martin on 17/06/2014.
+//  Copyright (c) 2014 Charles Martin. All rights reserved.
+//
+
+#import "MetatoneEnsembleView.h"
+
+@implementation MetatoneEnsembleView
+
+
+-(id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        // Init code.
+    }
+    return self;
+}
+
+@end
